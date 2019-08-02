56ee84042483b61cfc897442f645144966d525d4
Anton Bashirov <Anton.Bashirov@rt.ru>
2019-08-02 09:34:42 +0300

    added gradlew

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,13 @@ import org.jetbrains.kotlin.gradle.tasks.*
 
 val compileKotlin: KotlinCompile by tasks
 val compileTestKotlin: KotlinCompile by tasks
-val uploadArchives: Upload by tasks
 val jar: Jar by tasks
 val embedded by configurations.creating
 
-configurations.compileClasspath.get().extendsFrom(embedded)
+
+tasks.withType<Wrapper> {
+    gradleVersion = "5.5.1"
+}
 
 plugins {
     checkstyle
@@ -43,7 +45,7 @@ dependencies {
             .exclude("ch.qos.logback", "logback-classic")
             .exclude("org.codehaus.groovy", "groovy")
     embedded("org.eclipse.jgit", "org.eclipse.jgit", "5.3.1.201904271842-r")
-    embedded("gradle.plugin.com.github.lkishalmi.gatling", "gradle-gatling-plugin", "3.0.+")
+    embedded("gradle.plugin.com.github.lkishalmi.gatling", "gradle-gatling-plugin", "3.0.2")
     embedded("me.champeau.gradle", "jmh-gradle-plugin", "0.4.+")
     embedded("org.jetbrains.kotlin", "kotlin-gradle-plugin", "1.3.31")
     embedded("com.google.protobuf", "protobuf-gradle-plugin", "0.8+")
