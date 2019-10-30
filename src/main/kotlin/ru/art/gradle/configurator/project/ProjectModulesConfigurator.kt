f8b170c3840443b09d23a209d09b019fdc115e9c
antonbashir <antonbashir@mail.ru>
2019-10-30 05:09:36 +0300

    refactoring

--- src/main/kotlin/ru/art/gradle/configurator/project/ProjectModulesConfigurator.kt
+++ src/main/kotlin/ru/art/gradle/configurator/project/ProjectModulesConfigurator.kt
@@ -30,7 +30,7 @@ fun Project.addModules() {
     val providedModulesConfiguration = projectExtension.providedModulesConfiguration
     val testModulesConfiguration = projectExtension.testModulesConfiguration
 
-    if (projectExtension().generatorConfiguration.packageName.isNotBlank()) {
+    if (projectExtension().generatorConfiguration.packageName.isNotBlank() || projectExtension().generatorConfiguration.soapConfiguration.packageName.isNotBlank()) {
         providedModulesConfiguration.applicationCore()
         providedModulesConfiguration.applicationEntity()
         providedModulesConfiguration.applicationService()
