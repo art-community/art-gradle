1df3c470a3fef5c4408bea0a54d4a7b37c96e2cc
anton.bashirov <anton.sh.local@gmail.com>
2021-04-12 07:22:52 +0300

    ART

--- src/main/kotlin/io/art/gradle/internal/RepositorySelector.kt
+++ src/main/kotlin/io/art/gradle/internal/RepositorySelector.kt
@@ -4,7 +4,7 @@ import org.eclipse.jgit.api.Git
 import org.gradle.api.Project
 
 val Project.artifactsRepositoryUrl
-    get() = when (Git.open(projectDir).repository.branch) {
+    get() = when (Git.open(rootDir).repository.branch) {
         MAIN_BRANCH -> STABLE_MAVEN_REPOSITORY
         else -> UNSTABLE_MAVEN_REPOSITORY
     }
