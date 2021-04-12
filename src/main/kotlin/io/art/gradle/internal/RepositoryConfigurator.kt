9f0415f2018b3adc684d24e4fa009dfcd8fd8651
anton.bashirov <anton.sh.local@gmail.com>
2021-04-12 07:09:43 +0300

    ART

--- src/main/kotlin/io/art/gradle/internal/RepositoryConfigurator.kt
+++ src/main/kotlin/io/art/gradle/internal/RepositoryConfigurator.kt
@@ -3,8 +3,10 @@ package io.art.gradle.internal
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.repositories
 
-fun Project.configureRepositories() = repositories {
-    maven {
-        url = uri(artifactsRepositoryUrl)
+fun Project.configureRepositories() = allprojects {
+    repositories {
+        maven {
+            url = uri(artifactsRepositoryUrl)
+        }
     }
 }
