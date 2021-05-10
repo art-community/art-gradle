649a808bc9c0a6fbe0fc4989fd511eaff288c682
anton.bashirov <anton.sh.local@gmail.com>
2021-05-10 14:20:24 +0300

    ART

--- src/main/kotlin/io/art/gradle/external/configurator/NativeExecutableConfigurator.kt
+++ src/main/kotlin/io/art/gradle/external/configurator/NativeExecutableConfigurator.kt
@@ -46,6 +46,20 @@ fun Project.configureNative() {
             inputs.files(jarTask.outputs)
 
             val graalPaths = downloadGraal(native)
+            directory.resolve(GRAAL).toFile().apply {
+                mkdirs()
+                GRAAL_CONFIGURATION_FILES.forEach { json ->
+                    resolve(CONFIGURATION).apply {
+                        mkdir()
+                        val bytes = externalPlugin
+                                .javaClass
+                                .classLoader
+                                .getResourceAsStream(GRAAL_BASE_RESOURCE_CONFIGURATION_PATH(native.graalJavaVersion, json))!!
+                                .readBytes()
+                        resolve(json).writeBytes(bytes)
+                    }
+                }
+            }
             when {
                 OperatingSystem.current().isWindows -> useWindowsBuilder(this@with, graalPaths)
                 else -> useUnixBuilder(this@with, graalPaths)
@@ -111,46 +125,47 @@ private fun Project.downloadGraal(configuration: NativeExecutableConfiguration):
 }
 
 private fun Exec.useWindowsBuilder(configuration: ExecutableConfiguration, paths: GraalPaths) = with(configuration) {
-    directory.resolve(GRAAL).toFile().apply {
-        mkdirs()
-        GRAAL_CONFIGURATION_FILES.forEach { json ->
-            resolve(CONFIGURATION).apply {
-                mkdir()
-                val bytes = externalPlugin
-                        .javaClass
-                        .classLoader
-                        .getResourceAsStream(GRAAL_BASE_RESOURCE_CONFIGURATION_PATH(configuration.native.graalJavaVersion, json))!!
-                        .readBytes()
-                resolve(json).writeBytes(bytes)
-            }
-        }
-    }
-
     val executablePath = directory.resolve(configuration.executableName).toFile()
     val graalPath = directory.resolve(GRAAL)
     val configurationPath = graalPath.resolve(CONFIGURATION)
 
-    val options = listOf(
-            paths.nativeImage.absolutePath,
-            JAR_OPTION, directory.resolve("$executableName$DOT_JAR").toAbsolutePath().toString(),
-            executablePath.absolutePath,
-            GRAAL_PROXY_CONFIGURATION_OPTION(configurationPath.resolve(GRAAL_PROXY_CONFIGURATION)),
-            GRAAL_JNI_CONFIGURATION_OPTION(configurationPath.resolve(GRAAL_JNI_CONFIGURATION)),
-            GRAAL_REFLECTION_CONFIGURATION_OPTION(configurationPath.resolve(GRAAL_REFLECTION_CONFIGURATION)),
-            GRAAL_RESOURCE_CONFIGURATION_OPTION(configurationPath.resolve(GRAAL_RESOURCE_CONFIGURATION))
-    ) + native.graalOptions
-
     graalPath.resolve(GRAAL_WINDOWS_LAUNCH_SCRIPT_NAME).toFile().apply {
+        val options = listOf(
+                paths.nativeImage.absolutePath,
+                JAR_OPTION, directory.resolve("$executableName$DOT_JAR").toAbsolutePath().toString(),
+                executablePath.absolutePath,
+                GRAAL_PROXY_CONFIGURATION_OPTION(configurationPath.resolve(GRAAL_PROXY_CONFIGURATION)),
+                GRAAL_JNI_CONFIGURATION_OPTION(configurationPath.resolve(GRAAL_JNI_CONFIGURATION)),
+                GRAAL_REFLECTION_CONFIGURATION_OPTION(configurationPath.resolve(GRAAL_REFLECTION_CONFIGURATION)),
+                GRAAL_RESOURCE_CONFIGURATION_OPTION(configurationPath.resolve(GRAAL_RESOURCE_CONFIGURATION))
+        ) + native.graalOptions
+
         val scriptPath = native.graalWindowsVcVarsPath ?: throw graalWindowsVSVarsPathIsEmpty()
+
         writeText(GRAAL_WINDOWS_LAUNCH_SCRIPT(graalPath, scriptPath, options))
+
         commandLine(POWERSHELL, absolutePath)
     }
 
     outputs.files(executablePath)
 }
 
-private fun Project.useUnixBuilder(configuration: ExecutableConfiguration, paths: GraalPaths) {
+private fun Exec.useUnixBuilder(configuration: ExecutableConfiguration, paths: GraalPaths) = with(configuration) {
+    val executablePath = directory.resolve(configuration.executableName).toFile()
+    val graalPath = directory.resolve(GRAAL)
+    val configurationPath = graalPath.resolve(CONFIGURATION)
 
+    commandLine(paths.nativeImage.absolutePath)
+
+    args(native.graalOptions)
+    args(JAR_OPTION, directory.resolve("$executableName$DOT_JAR").toAbsolutePath().toString())
+    args(executablePath.absolutePath)
+    args(GRAAL_PROXY_CONFIGURATION_OPTION(configurationPath.resolve(GRAAL_PROXY_CONFIGURATION)))
+    args(GRAAL_JNI_CONFIGURATION_OPTION(configurationPath.resolve(GRAAL_JNI_CONFIGURATION)))
+    args(GRAAL_REFLECTION_CONFIGURATION_OPTION(configurationPath.resolve(GRAAL_REFLECTION_CONFIGURATION)))
+    args(GRAAL_RESOURCE_CONFIGURATION_OPTION(configurationPath.resolve(GRAAL_RESOURCE_CONFIGURATION)))
+
+    outputs.files(executablePath)
 }
 
 private data class GraalPaths(val base: File, val binary: File, val nativeImage: File)
