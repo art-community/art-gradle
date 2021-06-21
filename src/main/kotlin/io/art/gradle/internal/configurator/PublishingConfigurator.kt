1f8bac26bf22107b931946da84657db24b2a476a
anton.bashirov <anton.sh.local@gmail.com>
2021-06-21 04:46:38 +0300

    ART

--- src/main/kotlin/io/art/gradle/internal/configurator/PublishingConfigurator.kt
+++ src/main/kotlin/io/art/gradle/internal/configurator/PublishingConfigurator.kt
@@ -1,7 +1,9 @@
 package io.art.gradle.internal.configurator
 
+import io.art.gradle.common.constants.DOT_JAR
+import io.art.gradle.common.constants.JAVA
 import io.art.gradle.internal.constants.*
-import io.art.gradle.common.constants.*
+import io.art.gradle.internal.plugin.jvmPlugin
 import io.art.gradle.internal.service.loadProperty
 import io.art.gradle.internal.service.publishingRepositoryUrl
 import org.gradle.api.Project
@@ -17,7 +19,7 @@ fun Project.configurePublishing() {
     val publisherPassword = loadProperty(PUBLISHER_PASSWORD, PUBLISHING_PROPERTIES) ?: return
     when (rootProject.name) {
         ART_JAVA -> mavenPublishing(publisherUsername, publisherPassword)
-        ART_GENERATOR -> mavenPublishing(publisherUsername, publisherPassword)
+        ART_GENERATOR -> generatorPublishing(publisherUsername, publisherPassword)
         ART_KOTLIN -> mavenPublishing(publisherUsername, publisherPassword)
         ART_EXAMPLE -> mavenPublishing(publisherUsername, publisherPassword)
     }
@@ -44,15 +46,46 @@ private fun Project.mavenPublishing(publisherUsername: String, publisherPassword
                         groupId = rootProject.group as String
                         version = rootProject.version as String
                         from(project.components[JAVA])
-                        pom {
-                            name.set(project.name)
-                            url.set(gitUrl)
-                            licenses { licenses(this) }
-                            developers { developers(this) }
-                            scm { scm(this) }
-                            versionMapping { versionMapping(this) }
-                        }
+                        projectPom(this)
                     }
+                }
+            }
+        }
+    }
+}
+
+private fun Project.projectPom(mavenPublication: MavenPublication) {
+    mavenPublication.pom {
+        name.set(project.name)
+        url.set(gitUrl)
+        licenses { licenses(this) }
+        developers { developers(this) }
+        scm { scm(this) }
+        mavenPublication.versionMapping { versionMapping(this) }
+    }
+}
+
+private fun Project.generatorPublishing(publisherUsername: String, publisherPassword: String) {
+    plugins.apply(MavenPublishPlugin::class.java)
+    with(the<PublishingExtension>()) {
+        repositories {
+            maven {
+                url = uri(publishingRepositoryUrl)
+                credentials {
+                    username = publisherUsername
+                    password = publisherPassword
+                }
+            }
+        }
+        publications {
+            afterEvaluate {
+                create<MavenPublication>(project.name) {
+                    withoutBuildIdentifier()
+                    artifactId = project.name
+                    groupId = rootProject.group as String
+                    version = rootProject.version as String
+                    artifact(jvmPlugin.executable.directory.resolve("$name.$DOT_JAR"))
+                    projectPom(this)
                 }
             }
         }
