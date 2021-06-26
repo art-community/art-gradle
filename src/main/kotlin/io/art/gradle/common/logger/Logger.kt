20dc42a9498ebfb5c7e3e5d2abe856d66e466a11
anton.bashirov <anton.sh.local@gmail.com>
2021-06-27 01:01:31 +0300

    ART

--- src/main/kotlin/io/art/gradle/common/logger/Logger.kt
+++ src/main/kotlin/io/art/gradle/common/logger/Logger.kt
@@ -51,19 +51,19 @@ fun Project.quiet(message: String, context: String = project.name, color: LogMes
     logger.quiet("${color.code}${LOG_TEMPLATE(context, message)}$ANSI_RESET")
 }
 
-fun Project.success(message: String, context: String = project.name, color: LogMessageColor = LogMessageColor.GREEN_BOLD) {
+fun Project.success(message: String, context: String = project.name, color: LogMessageColor = GREEN_BOLD) {
     logger.quiet("${color.code}${LOG_TEMPLATE(context, message)}$ANSI_RESET")
 }
 
-fun Project.warning(message: String, context: String = project.name, color: LogMessageColor = LogMessageColor.YELLOW_BOLD) {
+fun Project.warning(message: String, context: String = project.name, color: LogMessageColor = YELLOW_BOLD) {
     logger.quiet("${color.code}${LOG_TEMPLATE(context, message)}$ANSI_RESET")
 }
 
-fun Project.attention(message: String, context: String = project.name, color: LogMessageColor = LogMessageColor.CYAN_BOLD) {
+fun Project.attention(message: String, context: String = project.name, color: LogMessageColor = CYAN_BOLD) {
     logger.quiet("${color.code}${LOG_TEMPLATE(context, message)}$ANSI_RESET")
 }
 
-fun Project.additional(message: String, context: String = project.name, color: LogMessageColor = LogMessageColor.PURPLE_BOLD) {
+fun Project.additional(message: String, context: String = project.name, color: LogMessageColor = PURPLE_BOLD) {
     logger.quiet("${color.code}${LOG_TEMPLATE(context, message)}$ANSI_RESET")
 }
 
