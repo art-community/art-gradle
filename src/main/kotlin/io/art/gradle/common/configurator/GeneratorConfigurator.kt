7651a7317abfd51c8d12d0bc3e40ffc23b354aed
Anton Bashirov <anton.bashirov@rt.ru>
2021-07-26 14:55:03 +0300

    ART

--- src/main/kotlin/io/art/gradle/common/configurator/GeneratorConfigurator.kt
+++ src/main/kotlin/io/art/gradle/common/configurator/GeneratorConfigurator.kt
@@ -228,8 +228,8 @@ private fun Project.collectJvmSources(configuration: GeneratorConfiguration): Se
 private fun Project.collectClasspath(): String {
     val classpath = mutableSetOf<File>()
     classpath += configurations
-            .filter { configuraton -> configuraton.name.startsWith(COMPILE_CLASS_PATH_CONFIGURATION_NAME) }
-            .flatMap { configuraton -> configuraton.files }
+            .filter { configuration -> configuration.name.endsWith(COMPILE_CLASS_PATH_CONFIGURATION_NAME) }
+            .flatMap { configuration -> configuration.files }
     if (OperatingSystem.current().isWindows) {
         return classpath.joinToString(SEMICOLON)
     }
