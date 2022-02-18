332de01b2a68ff34bd34785638d0ea1b7652aace
anton.bashirov <anton.sh.local@gmail.com>
2022-02-19 01:07:34 +0300

    ART

--- src/main/kotlin/io/art/gradle/common/constants/SourceDependencyConstaints.kt
+++ src/main/kotlin/io/art/gradle/common/constants/SourceDependencyConstaints.kt
@@ -2,9 +2,7 @@ package io.art.gradle.common.constants
 
 import UnixSourceDependency
 
-const val AUTOGEN_FILE = "autogen.sh"
 const val AUTOGEN_SCRIPT = "./autogen.sh"
-const val CONFIGURE_FILE = "configure"
 const val CONFIGURE_SCRIPT = "./configure"
 const val MAKE = "make"
 const val MAKE_FILE = "Makefile"
