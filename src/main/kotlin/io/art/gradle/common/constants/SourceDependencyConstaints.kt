f7a974cc19306e2caaab9871c572e475de4a727c
anton.bashirov <anton.sh.local@gmail.com>
2022-02-19 03:30:54 +0300

    ART

--- src/main/kotlin/io/art/gradle/common/constants/SourceDependencyConstaints.kt
+++ src/main/kotlin/io/art/gradle/common/constants/SourceDependencyConstaints.kt
@@ -27,7 +27,7 @@ const val DOS_TO_UNIX_FILE = "test /usr/bin/dos2unix && /usr/bin/dos2unix "
 fun preconfiguredLxc(static: Boolean) = UnixSourceDependency("lxc").apply {
     url("https://github.com/lxc/lxc")
     configureOptions("--disable-doc")
-    if (static) configureOptions("--enable-static")
+    if (static) configureOptions("--enable-static-binaries")
     parallel()
     copy("src/lxc/.libs/liblxc.a", "src/main/resources")
 }
